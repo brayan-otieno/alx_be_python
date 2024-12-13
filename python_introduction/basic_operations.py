be0c1482298dd37cae9745bdc197b7bb50e7fade
Mr Parrot <briandan2058@gmail.com>
2024-12-13 15:53:34 +0300

    Add basic_operations.py to python_introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# Define variables
+
+number1 = 10
+number2 = 5
+
+# Calculate sum
+sum_values = number1 + number2
+# Calculate subtraction
+diffference_values = number1 - number2
+# Calculate multiplication
+product_values = number1 * number2
+
+# Print the results
+print("Addition of", number1, "and", number2, "is", sum_values)
+print("Subtraction of", number1, "and", number2, "is", diffference_values)
+print("Multiplication of", number1, "and", number2, "is", product_values)
